c8f08a870121a726abc2bacbdf2817c71fc14fa7
saxenaharsh <harsh.saxena@gmail.com>
2019-06-24 17:43:49 +0530

    Add files via upload

---
+++ CodeOrganisation/src/com/HarshSaxena/Main.java
@@ -0,0 +1,16 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is another " +
+                "test" +
+                " still more");
+    }
+}
